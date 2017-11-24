9ccddec7e07f0ab7efa7862aacb9e85a8213970c
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2017-11-24 14:15:05 -0200

    Update user.js

--- app/models/user.js
+++ app/models/user.js
@@ -16,14 +16,14 @@ var UserSchema = Schema({
 
     provider_id: { type: String, trim: true },
     provider: { type: String, trim: true },
-    name: { type: String, trim: true },
+    name: { type: String, trim: true, lowercase: true },
     age: { type: Number, trim: true },
-    gender: { type: String, trim: true },
+    gender: { type: String, trim: true, lowercase: true },
     password: { type: String, trim: true },
-    email: { type: String, trim: true },
+    email: { type: String, trim: true, lowercase: true },
 
-    hability: { type: String, trim: true },
-    biography: { type: String, trim: true },
+    hability: { type: String, trim: true, lowercase: true },
+    biography: { type: String, trim: true, lowercase: true },
     photo: { type: String, trim: true },
     cover: { type: String, trim: true },
 
@@ -33,10 +33,10 @@ var UserSchema = Schema({
     soundCloud: { type: String, trim: true },
     facebook: { type: String, trim: true },
 
-    city: { type: String, trim: true },
-    state: { type: String, trim: true },
-    nation: { type: String, trim: true },
-    tags: [{ type: String }],
+    city: { type: String, trim: true, lowercase: true },
+    state: { type: String, trim: true, lowercase: true },
+    nation: { type: String, trim: true, lowercase: true },
+    tags: [{ type: String, lowercase: true }],
     token: { type: String },
     created_at: { type: Date, default: Date.valueOf() }
 });
