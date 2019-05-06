92a4dbab8f534a3b5a166205f6b880d72d691c74
Tassio Rocha <tassiorocha@ioasys.com.br>
2019-05-06 17:23:38 -0300

    user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,2 +1,13 @@
 const mongoose = require('mongoose');
 
+const Schema = mongoose.Schema;
+
+const UserSchema = Schema({
+    name        :   { type: String, trim: true },
+    password_hash    :   { type: String, trim: true },
+    email       :   { type: String, trim: true },
+    created_at  :   { type: Date, default: Date.valueOf() }  
+});
+
+//documentar estrutura de dados
+export default mongoose.model('User', UserSchema);
